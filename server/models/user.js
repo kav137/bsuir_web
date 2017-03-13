9960f2e8055965cf82bf6882deb0bcc696033c28
Artyom <artyom.koval.137@gmail.com>
2017-03-13 23:04:59 +0300

    API. Updated User mongo model

--- server/models/user.js
+++ server/models/user.js
@@ -1,49 +1,45 @@
 import mongoose from 'mongoose';
 
-let userSchema = mongoose.Schema( {
+let userSchema = mongoose.Schema({
 	id: Number,
 	username: String,
 	password: String
 });
 
-userSchema.methods.validatePassword = ( password ) => {
-	return ( this.password === password );
+userSchema.methods.validatePassword = (password) => {
+	return (this.password === password);
 };
 
-userSchema.methods.getToken = () => {
-	return Promise.resolve( this._id );
-};
+let User = mongoose.model('User', userSchema);
 
-let User = mongoose.model( 'User', userSchema );
-
-let getToken = ( user ) => {
+let getToken = (user) => {
 	return user.getToken();
 };
 
-let createUser = ( username, password ) => {
-	return new Promise(( resolve, reject ) => {
-		User.find( { username }, ( err, res ) => {
-			if ( err ) {
-				reject();
+let createUser = (username, password) => {
+	return new Promise((resolve, reject) => {
+		User.find({ username }, (err, res) => {
+			if (err) {
+				reject(new Error(`error accquired while checking whether user ${username} exists`));
 			}
-			if ( res && res.length ) {
-				console.log( `user ${username} already exists` );
-				reject();
+			if (res && res.length) {
+				console.log(`user ${username} already exists`);
+				reject(new Error(`user ${username} already exists`));
 			}
 			else {
 				resolve();
 			}
 		});
 	}).then(() => {
-		let newUser = new User( { username, password });
-		return new Promise(( resolve, reject ) => {
-			newUser.save(( err, result ) => {
-				if ( err ) {
-					console.log( `error during saving item ${newUser.username}` );
-					reject( err );
+		let newUser = new User({ username, password });
+		return new Promise((resolve, reject) => {
+			newUser.save((err, result) => {
+				if (err) {
+					console.log(`error during saving item ${newUser.username}`);
+					reject(err);
 				}
-				console.log( `successfully saved ${newUser.username}` );
-				resolve( result );
+				console.log(`successfully saved ${newUser.username}`);
+				resolve(result);
 			});
 		});
 	});
@@ -56,45 +52,45 @@ let insertInitialData = () => {
 		{ username: 'nick', password: 'web' }
 	];
 
-	return Promise.all( users.map(( user ) => {
-		return createUser( user.username, user.password );
-	}) );
+	return Promise.all(users.map((user) => {
+		return createUser(user.username, user.password);
+	}));
 };
 
 let deleteAll = () => {
-	return new Promise(( resolve, reject ) => {
-		User.remove( {}, ( err ) => {
-			if ( err ) {
-				console.log( 'there was a error during removing. failed' );
+	return new Promise((resolve, reject) => {
+		User.remove({}, (err) => {
+			if (err) {
+				console.log('there was a error during removing. failed');
 				reject();
 			}
-			console.log( 'all users were removed' );
+			console.log('all users were removed');
 			resolve();
 		});
 	});
 };
 
-let findOne = ( conditions ) => {
-	console.log( 'searching' );
-	console.log( conditions );
-	return new Promise(( resolve, reject ) => {
-		User.findOne( conditions, ( err, result ) => {
-			if ( err ) {
-				reject( new Error( 'error during search' ) );
+let findOne = (conditions) => {
+	console.log('searching');
+	console.log(conditions);
+	return new Promise((resolve, reject) => {
+		User.findOne(conditions, (err, result) => {
+			if (err) {
+				reject(new Error('error during search'));
 			}
-			resolve( result );
+			resolve(result);
 		});
 	});
 };
 
 let getAllData = () => {
-	return new Promise(( resolve, reject ) => {
-		User.find( {}, ( err, result ) => {
-			if ( err ) {
-				console.log( 'there was a error during data retrieving' );
-				reject();
+	return new Promise((resolve, reject) => {
+		User.find({}, (err, result) => {
+			if (err) {
+				console.log('there was a error during data retrieving');
+				reject(new Error('there was a error during data retrieving'));
 			}
-			resolve( result );
+			resolve(result);
 		});
 	});
 };
