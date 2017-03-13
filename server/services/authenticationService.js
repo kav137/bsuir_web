77f20f48da8477aebbe6927d6c033c12cc694fcd
Artyom <artyom.koval.137@gmail.com>
2017-03-13 23:15:58 +0300

    Refactoring. Updated obsolete links
    Replaced obsolete links with new direct ones

--- server/services/authenticationService.js
+++ server/services/authenticationService.js
@@ -1,11 +1,11 @@
 import passport from 'passport';
 import { Strategy } from 'passport-local';
-import models from '../models/index';
+import user from '../models/user';
 
 let init = () => {
 	passport.use(new Strategy(
 		function (username, password, done) {
-			models.user.findOne({ username, password }).then((foundUser) => {
+			user.findOne({ username, password }).then((foundUser) => {
 				return done(null, foundUser);
 			}).catch((error) => {
 				return done(null, false, error);
@@ -13,12 +13,12 @@ let init = () => {
 		})
 	);
 
-	passport.serializeUser((user, cb) => {
-		cb(null, user._id);
+	passport.serializeUser((sessionUser, cb) => {
+		cb(null, sessionUser._id);
 	});
 
 	passport.deserializeUser((id, cb) => {
-		models.user.findOne({ _id: id }).then((user) => {
+		user.findOne({ _id: id }).then((user) => {
 			cb(null, user);
 		});
 	});
